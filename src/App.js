8966ac28f9a24d790327a03de908784fe4ac89a6
Yvonne61m <yvonne61m@gmail.com>
2019-04-24 00:15:08 -0700

    changed css. add form.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Form from "react-jsonschema-form";
+import JSONSchemaForm from "react-jsonschema-form";
 import * as sample from './skillItUp.json';
 import { Editor } from 'react-draft-wysiwyg';
 import { convertFromRaw } from 'draft-js';
@@ -8,7 +9,7 @@ import '../node_modules/react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
 
 
 const log = (type) => console.log.bind(console, type);
-const content = {"entityMap":{},"blocks":[{"key":"637gr","text":"Initialized from content state.","type":"unstyled","depth":0,"inlineStyleRanges":[],"entityRanges":[],"data":{}}]};
+const content = { "entityMap": {}, "blocks": [{ "key": "637gr", "text": "Initialized from content state.", "type": "unstyled", "depth": 0, "inlineStyleRanges": [], "entityRanges": [], "data": {} }] };
 
 class App extends Component {
 
@@ -34,7 +35,7 @@ class App extends Component {
       properties: {
         id: { type: "number", title: "id", default: sample.id },
         title: { type: "string", title: "title", default: sample.title },
-        introduction: { type: "string", title: "introduction", default: sample.introduction},
+        introduction: { type: "string", title: "introduction", default: sample.introduction },
         criterionId: { type: "number", title: "criterionId", default: sample.criterionId },
         dtCreated: { type: "string", title: "dtCreated", default: sample.dtCreated },
         dtUpdated: { type: "string", title: "dtUpdated", default: sample.dtUpdated },
@@ -42,7 +43,9 @@ class App extends Component {
       }
     };
     const { contentState } = this.state;
-    const onSubmit = ({formData}, e) => console.log("Data submitted: ", formData)
+    const onSubmit = ({ formData }, e) => console.log("Data submitted: ", formData)
+
+
     return (
       <div className="App">
         <header className="App-header">
@@ -50,35 +53,35 @@ class App extends Component {
             onChange={console.log("changed")}
             // onSubmit={onSubmit} ref={(form) => {yourForm = form;}}
             onSubmit={onSubmit}
-            onError={log("errors")} />
+            onError={log("errors")}
+             />
 
-            
-    {
-      this.state.showHTMLEditor && 
-      <div>
-        <Editor
-          wrapperClassName="demo-wrapper"
-          editorClassName="demo-editor"
-          onContentStateChange={this.onContentStateChange}
-        />
-        <textarea
-          disabled
-          value={JSON.stringify(contentState, null, 4)}
-        />
-      </div>
-    }
-    <button onClick = {()=> {this.setState({showHTMLEditor: !this.state.showHTMLEditor})}}>Show Editor</button>
+
+          {
+            this.state.showHTMLEditor &&
+            <div>
+              <Editor
+                wrapperClassName="demo-wrapper"
+                editorClassName="demo-editor"
+                onContentStateChange={this.onContentStateChange}
+              />
+              <textarea
+                disabled
+                value={JSON.stringify(contentState, null, 4)}
+              />
+            </div>
+          }
+          <button onClick={() => { this.setState({ showHTMLEditor: !this.state.showHTMLEditor }) }}>Show Editor</button>
 
         </header>
       </div>
     )
   }
-  
-    
 
 
-    }
 
+
+}
 
 
 
