7d30c9c4a1bbf39920c317e20c3dc2179ec51f50
ziyuguo716 <guoziyu0716@hotmail.com>
2019-04-23 22:47:44 -0700

    textbox added with show editor button working

--- src/App.js
+++ src/App.js
@@ -2,18 +2,31 @@ import React, { Component } from 'react';
 import './App.css';
 import Form from "react-jsonschema-form";
 import * as sample from './skillItUp.json';
-
+import { Editor } from 'react-draft-wysiwyg';
+import { convertFromRaw } from 'draft-js';
+import '../node_modules/react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
 
 
 const log = (type) => console.log.bind(console, type);
+const content = {"entityMap":{},"blocks":[{"key":"637gr","text":"Initialized from content state.","type":"unstyled","depth":0,"inlineStyleRanges":[],"entityRanges":[],"data":{}}]};
 
 class App extends Component {
-  constructor(){
-    super()
+
+  constructor(props) {
+    super(props);
+    const contentState = convertFromRaw(content);
     this.state = {
+      contentState,
       showHTMLEditor: false
-    }
+    };
   }
+
+  onContentStateChange: Function = (contentState) => {
+    this.setState({
+      contentState,
+    });
+  };
+
   render() {
     const schema = {
       title: "SkillItUp",
@@ -28,9 +41,8 @@ class App extends Component {
         // done: { type: "boolean", title: "Done?", default: false }
       }
     };
-  
+    const { contentState } = this.state;
     const onSubmit = ({formData}, e) => console.log("Data submitted: ", formData)
-
     return (
       <div className="App">
         <header className="App-header">
@@ -39,15 +51,36 @@ class App extends Component {
             // onSubmit={onSubmit} ref={(form) => {yourForm = form;}}
             onSubmit={onSubmit}
             onError={log("errors")} />
+
+            
     {
-      this.state.showHTMLEditor && <p>I'm HTML Editor</p>
+      this.state.showHTMLEditor && 
+      <div>
+        <Editor
+          wrapperClassName="demo-wrapper"
+          editorClassName="demo-editor"
+          onContentStateChange={this.onContentStateChange}
+        />
+        <textarea
+          disabled
+          value={JSON.stringify(contentState, null, 4)}
+        />
+      </div>
     }
     <button onClick = {()=> {this.setState({showHTMLEditor: !this.state.showHTMLEditor})}}>Show Editor</button>
 
         </header>
       </div>
     )
+  }
+  
+    
+
+
     }
-}
+
+
+
+
 
 export default App;
