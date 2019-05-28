bc4f9b063f11572cc8203cae36228a10511f4865
ziyuguo716 <guoziyu0716@hotmail.com>
2019-05-28 15:10:25 -0700

    get the nested forms ready with WYSIWYG. But looking uglygit add .

--- src/schema.js
+++ src/schema.js
@@ -11,190 +11,199 @@ import htmlToDraft from 'html-to-draftjs';
 import Wysiwyg from './wysiwyg.js';
 
 export const originalSchema = {
-    "definitions": {
-      "Thing": {
-        "type": "object",
-        "properties": {
-          "name": {
-            "type": "string",
-            "default": "Default name"
-          }
-        }
-      }
-    },
-    "type": "object",
-    "properties": {
-      "listOfStrings": {
-        "type": "array",
-        "title": "A list of strings",
-        "items": {
+  "definitions": {
+    "Thing": {
+      "type": "object",
+      "properties": {
+        "name": {
           "type": "string",
-          "default": "bazinga"
-        }
-      },
-      "multipleChoicesList": {
-        "type": "array",
-        "title": "A multiple choices list",
-        "items": {
-          "type": "string",
-          "enum": [
-            "foo",
-            "bar",
-            "fuzz",
-            "qux"
-          ]
-        },
-        "uniqueItems": true
-      },
-      "fixedItemsList": {
-        "type": "array",
-        "title": "A list of fixed items",
-        "items": [
-          {
-            "title": "A string value",
-            "type": "string",
-            "default": "<p>bild</p>"
-          },
-          {
-            "title": "a boolean value",
-            "type": "boolean"
-          }
-        ],
-        "additionalItems": {
-          "title": "Additional item",
-          "type": "number"
-        }
-      },
-      "minItemsList": {
-        "type": "array",
-        "title": "A list with a minimal number of items",
-        "minItems": 3,
-        "items": {
-          "$ref": "#/definitions/Thing"
-        }
-      },
-      "defaultsAndMinItems": {
-        "type": "array",
-        "title": "List and item level defaults",
-        "minItems": 5,
-        "default": [
-          "carp",
-          "trout",
-          "bream"
-        ],
-        "items": {
-          "type": "string",
-          "default": "unidentified"
-        }
-      },
-      "nestedList": {
-        "type": "array",
-        "title": "Nested list",
-        "items": {
-          "type": "array",
-          "title": "Inner list",
-          "items": {
-            "type": "string",
-            "default": "lorem ipsum"
-          }
-        }
-      },
-      "unorderable": {
-        "title": "Unorderable items",
-        "type": "array",
-        "items": {
-          "type": "string",
-          "default": "lorem ipsum"
-        }
-      },
-      "unremovable": {
-        "title": "Unremovable items",
-        "type": "array",
-        "items": {
-          "type": "string",
-          "default": "lorem ipsum"
-        }
-      },
-      "noToolbar": {
-        "title": "No add, remove and order buttons",
-        "type": "array",
-        "items": {
-          "type": "string",
-          "default": "lorem ipsum"
-        }
-      },
-      "fixedNoToolbar": {
-        "title": "Fixed array without buttons",
-        "type": "array",
-        "items": [
-          {
-            "title": "A number",
-            "type": "number",
-            "default": 42
-          },
-          {
-            "title": "A boolean",
-            "type": "boolean",
-            "default": false
-          }
-        ],
-        "additionalItems": {
-          "title": "A string",
-          "type": "string",
-          "default": "lorem ipsum"
+          "default": "Default name"
         }
       }
     }
+  },
+  "type": "object",
+  "properties": {
+    // "listOfStrings": {
+    //   "type": "array",
+    //   "title": "A list of strings",
+    //   "items": {
+    //     "type": "string",
+    //     "default": "bazinga"
+    //   }
+    // },
+    // "multipleChoicesList": {
+    //   "type": "array",
+    //   "title": "A multiple choices list",
+    //   "items": {
+    //     "type": "string",
+    //     "enum": [
+    //       "foo",
+    //       "bar",
+    //       "fuzz",
+    //       "qux"
+    //     ]
+    //   },
+    //   "uniqueItems": true
+    // },
+    // "fixedItemsList": {
+    //   "type": "array",
+    //   "title": "A list of fixed items",
+    //   "items": [
+    //     {
+    //       "title": "A string value",
+    //       "type": "string",
+    //       "default": "<p>bild</p>"
+    //     }
+    //     // ,
+    //     // {
+    //     //   "title": "a boolean value",
+    //     //   "type": "boolean"
+    //     // }
+    //   ]
+    //   // ,
+    //   // "additionalItems": {
+    //   //   "title": "Additional item",
+    //   //   "type": "number"
+    //   // }
+    // },
+    // "minItemsList": {
+    //   "type": "array",
+    //   "title": "A list with a minimal number of items",
+    //   "minItems": 3,
+    //   "items": {
+    //     "$ref": "#/definitions/Thing"
+    //   }
+    // },
+    // "defaultsAndMinItems": {
+    //   "type": "array",
+    //   "title": "List and item level defaults",
+    //   "minItems": 5,
+    //   "default": [
+    //     "carp",
+    //     "trout",
+    //     "bream"
+    //   ],
+    //   "items": {
+    //     "type": "string",
+    //     "default": "unidentified"
+    //   }
+    // },
+    "nestedList": {
+      "type": "array",
+      "title": "Chanllenges",
+      "items": {
+        "type": "array",
+        "title": "Questions",
+        "items": {
+          "type": "string",
+          "default": "<p>Edit your question here!</p>"
+        }
+      }
+    },
+    // "unorderable": {
+    //   "title": "Unorderable items",
+    //   "type": "array",
+    //   "items": {
+    //     "type": "string",
+    //     "default": "lorem ipsum"
+    //   }
+    // },
+    // "unremovable": {
+    //   "title": "Unremovable items",
+    //   "type": "array",
+    //   "items": {
+    //     "type": "string",
+    //     "default": "lorem ipsum"
+    //   }
+    // },
+    // "noToolbar": {
+    //   "title": "No add, remove and order buttons",
+    //   "type": "array",
+    //   "items": {
+    //     "type": "string",
+    //     "default": "lorem ipsum"
+    //   }
+    // },
+    // "fixedNoToolbar": {
+    //   "title": "Fixed array without buttons",
+    //   "type": "array",
+    //   "items": [
+    //     {
+    //       "title": "A number",
+    //       "type": "number",
+    //       "default": 42
+    //     },
+    //     {
+    //       "title": "A boolean",
+    //       "type": "boolean",
+    //       "default": false
+    //     }
+    //   ],
+    //   "additionalItems": {
+    //     "title": "A string",
+    //     "type": "string",
+    //     "default": "lorem ipsum"
+    //   }
+    // }
   }
-
-export const widgets = {
-  myWysiwyg : Wysiwyg
 }
 
-  export const originalUISchema = {
-    "listOfStrings": {
+export const widgets = {
+  myWysiwyg: Wysiwyg
+}
+
+export const originalUISchema = {
+  "nestedList": {
+    "items": {
       "items": {
-        "ui:emptyValue": ""
-      }
-    },
-    "multipleChoicesList": {
-      "ui:widget": "checkboxes"
-    },
-    "fixedItemsList": {
-      "items": [
-        {
-          "ui:widget": "myWysiwyg"
-        },
-        {
-          "ui:widget": "select"
-        }
-      ],
-      "additionalItems": {
-        "ui:widget": "updown"
-      }
-    },
-    "unorderable": {
-      "ui:options": {
-        "orderable": false
-      }
-    },
-    "unremovable": {
-      "ui:options": {
-        "removable": false
-      }
-    },
-    "noToolbar": {
-      "ui:options": {
-        "addable": false,
-        "orderable": false,
-        "removable": false
-      }
-    },
-    "fixedNoToolbar": {
-      "ui:options": {
-        "addable": false,
-        "orderable": false,
-        "removable": false
+        "ui:widget": "myWysiwyg"
       }
     }
+  },
+  "listOfStrings": {
+    "items": {
+      "ui:emptyValue": ""
+    }
+  },
+  "multipleChoicesList": {
+    "ui:widget": "checkboxes"
+  },
+  "fixedItemsList": {
+    "items": [
+      {
+        "ui:widget": "myWysiwyg"
+      },
+      {
+        "ui:widget": "select"
+      }
+    ],
+    "additionalItems": {
+      "ui:widget": "updown"
+    }
+  },
+  "unorderable": {
+    "ui:options": {
+      "orderable": false
+    }
+  },
+  "unremovable": {
+    "ui:options": {
+      "removable": false
+    }
+  },
+  "noToolbar": {
+    "ui:options": {
+      "addable": false,
+      "orderable": false,
+      "removable": false
+    }
+  },
+  "fixedNoToolbar": {
+    "ui:options": {
+      "addable": false,
+      "orderable": false,
+      "removable": false
+    }
   }
+}
